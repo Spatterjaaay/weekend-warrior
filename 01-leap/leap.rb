ccac1807c0b3703dd61b7a2e6c8b14bf19395ee8
Spatterjaaay <aja.pursova@gmail.com>
2017-02-20 18:52:04 -0800

    Added checks for divisible by 100 and 400

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,7 +1,26 @@
 def leap_year?(year)
+  # is not divisible by 4 and is divisible by 100
   if year % 4 == 0
-    true
+    if year % 100 == 0
+      if year % 400 == 0
+        return true
+      end
+      return false
+    end
+    return true
   else
-    false
+    return false
   end
 end
+
+# def leap_year?(year)
+#   if year % 400 == 0
+#     true
+#   elsif year % 100 == 0
+#     false
+#   elsif year % 4 == 0
+#     true
+#   else
+#     false
+#   end
+# end
