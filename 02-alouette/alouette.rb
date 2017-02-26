7aae49e31bd223febe5c4dc781a867491b4b8215
Spatterjaaay <aja.pursova@gmail.com>
2017-02-26 10:47:05 -0800

    Added class method sing

--- 02-alouette/alouette.rb
+++ 02-alouette/alouette.rb
@@ -1,6 +1,6 @@
 class Alouette
 
-    @lines = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+    @@lines = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
 
 
  # generating all those `Et <part>!` lines for a given verse
@@ -9,15 +9,16 @@ class Alouette
     verse_lines = []
 
     (number + 1).times do |index|
-      verse_lines.unshift "Et #{@lines[index]}!"
+      verse_lines.unshift "Et #{@@lines[index]}!"
     end
+
     return verse_lines
   end
 
   # build the requested verse
   # It should return a string
   def self.verse(number)
-    verse_a = "Je te plumerai #{@lines[number]}.\n" * 2
+    verse_a = "Je te plumerai #{@@lines[number]}.\n" * 2
     verse_b = ""
 
     lines_for_verse(number).each do |line|
@@ -29,14 +30,22 @@ class Alouette
 
 
     return verse_a + verse_b + verse_c + verse_d
-
-
   end
 
   # build the entire song, formatted
   # should be a blank line between verses and refrains
   # The value returned should return a string
   def self.sing
+    refrain = "Alouette, gentille alouette,\nAlouette, je te plumerai."
+    song = ""
+
+    8.times do | verse_number |
+      song += refrain + "\n\n" + verse(verse_number) + "\n\n"
+    end
+
+    song += refrain
+
+    return song
   end
 
 end
