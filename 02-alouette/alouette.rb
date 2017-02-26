e8f75367259b190a5f654b0f5b46d357a3bc4340
Spatterjaaay <aja.pursova@gmail.com>
2017-02-25 20:38:46 -0800

    Added lines_for_verse class method and lines variable

--- 02-alouette/alouette.rb
+++ 02-alouette/alouette.rb
@@ -1,15 +1,27 @@
 class Alouette
 
-  def initialize
+  @lines = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+
+ # generating all those `Et <part>!` lines for a given verse
+ # It should return an array of strings, without repeats
+  def self.lines_for_verse(number)
+    verse = []
+
+    number.times do |index|
+      verse.unshift "Et #{@lines[index -1]}!"
+    end
+    return verse
+  end
+  
+  # build the requested verse
+  # It should return a string
+  def self.verse
   end
 
-  def lines_for_verse
-  end
-
-  def verse
-  end
-
-  def sing
+  # build the entire song, formatted
+  # should be a blank line between verses and refrains
+  # The value returned should return a string
+  def self.sing
   end
 
 end
